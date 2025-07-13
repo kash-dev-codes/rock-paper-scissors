7794eab319a2de110e41d7a91b06c93b9c98c913
Awaji Kachi <kash_dev_25@outlook.com>
2025-07-13 16:42:29 +0100

    Step 5

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 let humanScore = 0;
 let computerScore = 0;
+let roundNo = 0;
+
 console.log('Hello, I\'m working');
 
 function getComputerChoice(){
@@ -23,13 +25,51 @@ function getComputerChoice(){
 function getUserChoice(){
   userChoice = prompt('Rock, Paper, or Scissors?');
   console.log(userChoice);
+  return userChoice;
 }
 
+function playRound(humanChoice, computerChoice){
+  winMsg = `You Win! ${humanChoice} beats ${computerChoice}`;
+  loseMsg = `You Lose! ${computerChoice} beats ${humanChoice}`;
+  cleanHC = humanChoice.toLowerCase();
 
-console.log(getComputerChoice());
-console.log(getComputerChoice());
-console.log(getComputerChoice());
-console.log(getComputerChoice());
-console.log(getComputerChoice());
+  if (computerChoice === 'rock'){
+    if (cleanHC === 'paper'){
+      ++humanScore;
+      console.log('You win');
+    }
+    else (cleanHC === 'scissors'){
+      ++computerScore;
+      console.log('Computer wins');
+    }}
+  else if (computerChoice === 'paper'){
+    if (cleanHC === 'rock'){
+      ++computerScore;
+      console.log('Computer Wins');
+    }
+    else if (cleanHC === 'scissors'){
+      ++humanScore;
+      console.log('You win');
+    }}
+  else if(computerChoice === 'scissors'){
+    if (cleanHC === 'rock'){
+      ++computerScore;
+      console.log('Computer Wins');
+    }
+    else if (cleanHC === 'paper'){
+      ++humanScore;
+      console.log('You win');
+    }}
+  else{
+    console.log('Draw!!');
+  }
 
-getUserChoice();
+}
+
+const compChoice = getComputerChoice();
+console.log(compChoice);
+
+const userChoice = getUserChoice;
+console.log(userChoice);
+
+playRound(userChoice, humanChoice);
